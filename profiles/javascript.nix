0ca38055f7b05baad85d7113ef7fee312c03b867
TehPers <tehperz@gmail.com>
2026-05-22 00:08:12 -0700

    Add option to install Boa CLI

--- profiles/javascript.nix
+++ profiles/javascript.nix
@@ -10,14 +10,22 @@ in
 {
   options.profiles.javascript = {
     enable = lib.mkEnableOption "Enable JavaScript profile";
+    enableBoa = lib.mkEnableOption "Enable the Boa runtime";
   };
 
-  config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      bun
-      fnm
-    ];
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
+      {
+        home.packages = with pkgs; [
+          bun
+          fnm
+        ];
 
-    programs.bun.enable = true;
-  };
+        programs.bun.enable = true;
+      }
+      (lib.mkIf cfg.enableBoa {
+        home.packages = with pkgs; [ boa ];
+      })
+    ]
+  );
 }
